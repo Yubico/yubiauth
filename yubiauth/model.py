ef19145fd2b3de675dbbbe4204a139d8d8f42627
Dain Nilsson <dain@yubico.com>
2013-03-27 11:44:58 +0100

    Updated object model.

--- yubiauth/model.py
+++ yubiauth/model.py
@@ -8,18 +8,98 @@ __all__ = [
 
 from config import settings
 
-from sqlalchemy import (create_engine, Sequence, Column, Integer,
-                        String, ForeignKey, UniqueConstraint)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import (create_engine, Sequence, Column, Boolean, Integer,
+                        String, ForeignKey, UniqueConstraint, Table)
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.orm.collections import attribute_mapped_collection
 from sqlalchemy.ext.associationproxy import association_proxy
 
+
 Base = declarative_base()
+
 pwd_context = settings['pwd_context']
 
 
-class User(Base):
+user_yubikeys = Table('user_yubikeys', Base.metadata,
+                      Column('user_id', Integer, ForeignKey('users.id')),
+                      Column('yubikey_id', Integer, ForeignKey('yubikeys.id'))
+                      )
+
+
+class AttributeAssociation(Base):
+    __tablename__ = 'attribute_associations'
+
+    id = Column(Integer, Sequence('attr_assoc_seq'), primary_key=True)
+    _discriminator = Column('discriminator', String(16))
+    _attributes = relationship(
+        'Attribute',
+        backref='association',
+        order_by='Attribute.key',
+        collection_class=attribute_mapped_collection('key'),
+        cascade='all, delete-orphan'
+    )
+    attributes = association_proxy(
+        '_attributes',
+        'value',
+        creator=lambda k, v: Attribute(k, v)
+    )
+
+    @property
+    def owner(self):
+        return getattr(self, '%s_owner' % self._discriminator)
+
+
+class Attribute(Base):
+    """
+    Holds an attribute for an AttributeHolder.
+
+    A user can have zero or more attributes, though each attribute must have
+    a unique key per user.
+    """
+    __tablename__ = 'attributes'
+    __table_args__ = (UniqueConstraint('association_id', 'key',
+                                       name='_owner_key_uc'),)
+
+    id = Column(Integer, Sequence('attribute_id_seq'), primary_key=True)
+    _association_id = Column('association_id', Integer, ForeignKey(
+        'attribute_associations.id'))
+    key = Column(String(32), nullable=False)
+    value = Column(String(128), nullable=False)
+    owner = association_proxy('association', 'owner')
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __repr__(self):
+        return ('%s = %s' % (self.key, self.value)).encode('utf-8')
+
+
+class AttributeHolder(object):
+    def __init__(self):
+        discriminator = self.__class__.__name__.lower()
+        self._attribute_association = AttributeAssociation(
+            _discriminator=discriminator)
+
+    @declared_attr
+    def _attribute_association_id(cls):
+        return Column('attribute_association_id', Integer, ForeignKey(
+            'attribute_associations.id'))
+
+    @declared_attr
+    def _attribute_association(cls):
+        discriminator = cls.__name__.lower()
+        cls.attributes = association_proxy(
+            '_attribute_association',
+            'attributes'
+        )
+        return relationship('AttributeAssociation', backref=backref(
+            '%s_owner' % discriminator, uselist=False), cascade=
+            'all, delete')
+
+
+class User(AttributeHolder, Base):
     """
     A user.
 
@@ -39,41 +119,39 @@ class User(Base):
     auth = Column(String(128))
     yubikeys = relationship(
         'YubiKey',
-        backref='user',
-        order_by='YubiKey.public_id',
-        collection_class=attribute_mapped_collection('public_id'),
-        cascade='all, delete-orphan'
-    )
-    _attributes = relationship(
-        'Attribute',
-        backref='user',
-        order_by='Attribute.key',
-        collection_class=attribute_mapped_collection('key'),
-        cascade='all, delete-orphan'
-    )
-    attributes = association_proxy(
-        '_attributes',
-        'value',
-        creator=lambda k, v: Attribute(k, v)
+        secondary=user_yubikeys,
+        backref='users',
+        order_by='YubiKey.prefix',
+        collection_class=attribute_mapped_collection('prefix'),
+        # cascade='all, delete-orphan'
     )
 
     def __init__(self, name, password):
         self.name = name
         self.set_password(password)
+        super(User, self).__init__()
 
-    def assign_yubikey(self, public_id_or_otp):
+    def assign_yubikey(self, prefix_or_otp):
         """
         Assigns a YubiKey to the user.
 
-        @param public_id_or_otp: The public ID of a YubiKey, or a full OTP.
-        @type public_id_or_otp: string
+        @param prefix_or_otp: The public ID of a YubiKey, or a full OTP.
+        @type prefix_or_otp: string
         """
-        if len(public_id_or_otp) > 32:
-            public_id = public_id_or_otp[:-32]
+        if len(prefix_or_otp) > 32:
+            prefix = prefix_or_otp[:-32]
         else:
-            public_id = public_id_or_otp
+            prefix = prefix_or_otp
 
-        self.yubikeys[public_id] = YubiKey(public_id)
+        session = Session.object_session(self)
+        existing_key = session.query(YubiKey).filter(
+            YubiKey.prefix == prefix).first()
+
+        if existing_key:
+            existing_key.users.append(self)
+        else:
+            self.yubikeys[prefix] = YubiKey(prefix)
+        session.commit()
 
     def set_password(self, password):
         """
@@ -114,10 +192,13 @@ class User(Base):
         to the user. False if not.
         @rtype: bool
         """
-        public_id = otp[:-32]
+        prefix = otp[:-32]
         otp_valid = True  # TODO: Validate OTP against YKVAL
 
-        return otp_valid and public_id in self.yubikeys
+        if prefix in self.yubikeys:
+            return otp_valid and self.yubikeys[prefix].enabled
+
+        return False
 
     @property
     def data(self):
@@ -129,11 +210,11 @@ class User(Base):
         }
 
     def __repr__(self):
-        return ("User(id: '%d', name: '%s', attributes: '%r')" %
+        return ("User(id: '%r', name: '%s', attributes: '%r')" %
                 (self.id, self.name, self.attributes)).encode('utf-8')
 
 
-class YubiKey(Base):
+class YubiKey(AttributeHolder, Base):
     """
     A reference to a YubiKey.
 
@@ -142,53 +223,29 @@ class YubiKey(Base):
     from the YubiKey.
 
     @cvar id: ID column.
-    @cvar public_id: YubiKey public ID column.
+    @cvar prefix: YubiKey public ID column.
     @cvar user_id: FK of the owner.
     """
     __tablename__ = 'yubikeys'
 
     id = Column(Integer, Sequence('yubikey_id_seq'), primary_key=True)
-    public_id = Column(String(32), nullable=False, unique=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    prefix = Column(String(32), nullable=False, unique=True)
+    enabled = Column(Boolean, default=True)
 
-    def __init__(self, public_id):
-        self.public_id = public_id
+    def __init__(self, prefix):
+        self.prefix = prefix
+        super(YubiKey, self).__init__()
 
     @property
     def data(self):
         return {
-            'public_id': self.public_id,
+            'prefix': self.prefix,
             'owner': self.user_id
         }
 
     def __repr__(self):
-        return ("YubiKey(id: '%d', user: '%s', public_id: '%s')" %
-                (self.id, self.user.name, self.public_id)).encode('utf-8')
-
-
-class Attribute(Base):
-    """
-    Holds an attribute for a user.
-
-    A user can have zero or more attributes, though each attribute must have
-    a unique key per user.
-    """
-    __tablename__ = 'attributes'
-    __table_args__ = (UniqueConstraint('user_id', 'key', name='_user_key_uc'),
-                      )
-
-    id = Column(Integer, Sequence('attribute_id_seq'), primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    key = Column(String(32), nullable=False)
-    value = Column(String(128), nullable=False)
-
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-    def __repr__(self):
-        return ("%s[%s] = %s" % (self.user.name, self.key, self.value)
-                ).encode('utf-8')
+        return ("YubiKey(id: '%r', prefix: '%s')" %
+                (self.id, self.prefix)).encode('utf-8')
 
 
 def create_db(engine):
