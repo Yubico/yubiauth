aa6a7eae8dfa042799ea07f2ae693a6c8205882f
Dain Nilsson <dain@yubico.com>
2013-04-02 16:53:08 +0200

    Better error checking

--- yubiauth/rest_api.py
+++ yubiauth/rest_api.py
@@ -48,10 +48,13 @@ ID_RE = re.compile(r'^%s$' % ID_PATTERN)
 class Route(object):
     def __init__(self, pattern_str, controller=None, get=None, post=None):
         self.pattern = re.compile(pattern_str)
+
         if controller:
             self.get = controller
-        else:
+            self.post = controller
+        if get:
             self.get = get
+        if post:
             self.post = post
 
     def get_controller(self, request):
@@ -65,12 +68,6 @@ class Route(object):
                 controller = self.__getattribute__(request.method.lower())
                 return controller, match.groups()
             except AttributeError:
-                print 'method not found!'
-                print path
-                print self.pattern.pattern
-                print request.method.lower()
-                print self.__dict__
-
                 raise exc.HTTPMethodNotAllowed
 
         return None, None
@@ -83,18 +80,19 @@ class WebAPI(object):
 
     __routes__ = [
         Route(r'^users$', get='list_users', post='create_user'),
-        Route(__user__ + r'$', 'show_user'),
-        Route(__user__ + r'/reset$', 'reset_password'),
-        Route(__user__ + r'/delete$', 'delete_user'),
+        Route(__user__ + r'$', get='show_user'),
+        Route(__user__ + r'/reset$', get='reset_password'),
+        Route(__user__ + r'/delete$', post='delete_user'),
         Route(__user__ + r'/attributes$', get='list_attributes',
               post='set_attribute'),
-        Route(__attribute__ + r'$', 'show_attribute'),
-        Route(__attribute__ + r'/delete$', 'unset_attribute'),
+        Route(__attribute__ + r'$', get='show_attribute'),
+        Route(__attribute__ + r'/delete$', post='unset_attribute'),
         Route(__user__ + r'/yubikeys$', get='list_yubikeys',
               post='bind_yubikey'),
-        Route(__yubikey__ + r'$', 'show_yubikey'),
-        Route(__yubikey__ + r'/delete$', 'unbind_yubikey'),
-        Route(r'^validate$', get='validate', post='validate')
+        Route(__yubikey__ + r'$', get='show_yubikey'),
+        Route(__yubikey__ + r'/delete$', post='unbind_yubikey'),
+        Route(r'^validate$', 'validate'),
+        Route(r'^authenticate$', 'authenticate')
     ]
 
     @wsgify
@@ -123,8 +121,11 @@ class WebAPI(object):
         return Response(json.dumps(self.auth.query_users()))
 
     def create_user(self, request):
-        username = request.params['username']
-        password = request.params['password']
+        try:
+            username = request.params['username']
+            password = request.params['password']
+        except KeyError:
+            raise exc.HTTPBadRequest
 
         user = self.auth.create_user(username, password)
 
@@ -136,7 +137,11 @@ class WebAPI(object):
 
     def reset_password(self, request, username_or_id):
         user = self._get_user(username_or_id)
-        password = request.params['password']
+        try:
+            password = request.params['password']
+        except KeyError:
+            raise exc.HTTPBadRequest
+
         user.set_password(password)
         self.auth.commit()
 
@@ -169,8 +174,12 @@ class WebAPI(object):
 
     def set_attribute(self, request, username_or_id):
         user = self._get_user(username_or_id)
-        key = request.params['key']
-        value = request.params['value']
+        try:
+            key = request.params['key']
+            value = request.params['value']
+        except KeyError:
+            raise exc.HTTPBadRequest
+
         user.attributes[key] = value
         self.auth.commit()
 
@@ -211,7 +220,10 @@ class WebAPI(object):
     # Validate
 
     def validate(self, request):
-        username = request.params['user']
+        try:
+            username = request.params['username']
+        except KeyError:
+            raise exc.HTTPBadRequest
         user = self.auth.get_user(username)
 
         if 'password' in request.params:
@@ -231,6 +243,22 @@ class WebAPI(object):
             'valid_password': valid_pass,
             'valid_otp': valid_otp
         }))
+
+    def authenticate(self, request):
+        try:
+            username = request.params['username']
+            password = request.params['password']
+        except KeyError:
+            raise exc.HTTPBadRequest
+
+        otp = request.params['otp'] if 'otp' in request.params else None
+
+        user = self.auth.authenticate(username, password, otp)
+
+        if user:
+            return Response(json.dumps(user.data))
+        raise exc.HTTPUnauthorized
+
 
 application = WebAPI()
 
