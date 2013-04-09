0688a6c5f5d4f9d469f685a333e29d154959352f
Dain Nilsson <dain@yubico.com>
2013-04-09 15:34:13 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if 'test' in sys.argv or 'nosetests' in sys.argv:
 
 setup(
     name='yubiauth',
-    version='0.1.1',
+    version='0.1.2',
     author='Dain Nilsson',
     author_email='dain@yubico.com',
     maintainer='Yubico Open Source Maintainers',
