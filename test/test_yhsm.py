01edea3e313e0d4291c701f8d002c714745affdc
Dain Nilsson <dain@yubico.com>
2013-04-10 15:04:47 +0200

    Removed test_yhsm pyhsm requirement unless run with -a hsm

--- test/test_yhsm.py
+++ test/test_yhsm.py
@@ -15,12 +15,19 @@ PASSWORDS = [
     chr(150) + chr(200) + chr(255)
 ]
 
-context = CryptContext(
-    schemes=['yhsm_pbkdf2_sha1', 'yhsm_pbkdf2_sha256', 'yhsm_pbkdf2_sha512'],
-    default='yhsm_pbkdf2_sha1',
-    all__key_handle=1,
-    all__rounds=10
-)
+context = None
+
+
+@attr(hsm=True)
+def setup():
+    global context
+    context = CryptContext(
+        schemes=['yhsm_pbkdf2_sha1', 'yhsm_pbkdf2_sha256',
+                 'yhsm_pbkdf2_sha512'],
+        default='yhsm_pbkdf2_sha1',
+        all__key_handle=1,
+        all__rounds=10
+    )
 
 
 def _algorithm_test(scheme):
