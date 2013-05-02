cafa741165dc52a21844160a60f5b435ed792779
Dain Nilsson <dain@yubico.com>
2013-05-02 13:20:01 +0200

    Added release command.

--- setup.py
+++ setup.py
@@ -31,14 +31,16 @@
 from setuptools import setup
 import sys
 import os
+from release import release
 
 tests_require = ['WebTest']
 
-# Don't load custom settings when running tests
-if 'test' in sys.argv or 'nosetests' in sys.argv:
-    os.environ['YUBIAUTH_SETTINGS'] = '/dev/null'
-    if 'hsm' in sys.argv:
-        tests_require.append('pyhsm')
+# Don't load custom settings (for tests, etc.)
+os.environ['YUBIAUTH_SETTINGS'] = '/dev/null'
+
+# Require pyhsm if running hsm tests
+if 'hsm' in sys.argv:
+    tests_require.append('pyhsm')
 
 setup(
     name='yubiauth',
@@ -54,6 +56,7 @@ setup(
     install_requires=['sqlalchemy', 'webob', 'passlib', 'yubico-client'],
     test_suite="nose.collector",
     tests_require=tests_require,
+    cmdclass={'release': release},
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
