960481720910bce7468e0d74003fef978ffd1598
Dain Nilsson <dain@yubico.com>
2013-05-23 15:01:25 +0200

    Fixed tests.

--- test/test_client_api.py
+++ test/test_client_api.py
@@ -1,5 +1,5 @@
 from webtest import TestApp
-from yubiauth.rest import application
+from yubiauth.client.rest import application
 from yubiauth.util.model import engine
 from yubiauth import create_tables, YubiAuth
 from utils import setting
@@ -20,108 +20,104 @@ def setup():
     auth.create_user('user2', 'pass2')
     auth.commit()
     del auth
-    # app.post('/yubiauth/core/users',
-    #         {'username': 'user1', 'password': 'pass1'})
-    # app.post('/yubiauth/core/users',
-    #         {'username': 'user2', 'password': 'pass2'})
 
 
 def test_login_logout():
-    sessionId = app.post(
-        '/yubiauth/client/login',
+    app.post(
+        '/login',
         {'username': 'user1', 'password': 'pass1'}
-    ).headers['X-YubiAuth-Session']
+    )
 
     # Uses the cookie set by the previous call
-    sessionId2 = app.get('/yubiauth/client/status').json['sessionId']
+    username = app.get('/status').json['username']
 
-    assert sessionId == sessionId2
+    assert username == 'user1'
 
-    app.get('/yubiauth/client/logout')
-    app.get('/yubiauth/client/status', status=400)
+    app.get('/logout')
+    app.get('/status', status=400)
 
 
 def test_update_password():
     app.post(
-        '/yubiauth/client/login',
+        '/login',
         {'username': 'user1', 'password': 'pass1'}
     )
 
-    app.post('/yubiauth/client/password', {
+    app.post('/password', {
         'oldpass': 'pass1', 'newpass': 'foobar'})
 
-    assert app.post('/yubiauth/client/authenticate',
+    assert app.post('/authenticate',
                     {'username': 'user1', 'password': 'foobar'}).json
 
-    app.post('/yubiauth/client/password', {
+    app.post('/password', {
         'oldpass': 'pass1', 'newpass': 'foobar'}, status=400)
 
-    app.post('/yubiauth/client/password', {
+    app.post('/password', {
         'oldpass': 'foobar', 'newpass': 'pass1'})
-    app.get('/yubiauth/client/logout')
+    app.get('/logout')
 
 
 def test_empty_password_login():
     app.post(
-        '/yubiauth/client/login',
+        '/login',
         {'username': 'user1', 'password': 'pass1'}
     )
 
-    app.post('/yubiauth/client/password', {
+    app.post('/password', {
         'oldpass': 'pass1', 'newpass': ''})
 
-    assert not app.post('/yubiauth/client/authenticate',
+    assert not app.post('/authenticate',
                         {'username': 'user1'}, status=400).json
 
     with setting(allow_empty=True):
-        assert app.post('/yubiauth/client/authenticate',
+        assert app.post('/authenticate',
                         {'username': 'user1'}).json
 
-        app.post('/yubiauth/client/password', {
+        app.post('/password', {
             'oldpass': '', 'newpass': 'pass1'})
 
-    app.get('/yubiauth/client/logout')
+    app.get('/logout')
 
 
 @patch('yubiauth.util.utils.yubico', return_value=True)
 def test_authentication_without_username(mock):
     app.post(
-        '/yubiauth/client/login',
+        '/login',
         {'username': 'user1', 'password': 'pass1'}
     )
 
     otp = 'c' * 44
-    app.post('/yubiauth/client/yubikey', {'yubikey': otp, 'password': 'pass1'})
+    app.post('/yubikey', {'yubikey': otp, 'password': 'pass1'})
 
-    assert not app.post('/yubiauth/client/authenticate',
+    assert not app.post('/authenticate',
                         {'otp': otp, 'password': 'pass1'},
                         status=400).json
 
     with setting(yubikey_id=True):
-        assert app.post('/yubiauth/client/authenticate',
+        assert app.post('/authenticate',
                         {'otp': otp, 'password': 'pass1'}).json
 
-        assert not app.post('/yubiauth/client/authenticate',
+        assert not app.post('/authenticate',
                             {'otp': otp, 'password': 'wrongpass'},
                             status=400).json
         otp = 'd' * 44
 
-        assert not app.post('/yubiauth/client/authenticate',
+        assert not app.post('/authenticate',
                             {'otp': otp, 'password': 'pass1'},
                             status=400).json
 
-    app.get('/yubiauth/client/logout')
+    app.get('/logout')
 
 
 @patch('yubiauth.util.utils.yubico', return_value=True)
 def test_single_factor_login(mock):
     otp = 'c' * 44
     with setting(yubikey_id=True, allow_empty=True):
-        assert app.post('/yubiauth/client/authenticate', {'otp': otp}).json
-        app.post('/yubiauth/client/login', {'otp': otp})
-        status = app.get('/yubiauth/client/status').json
+        assert app.post('/authenticate', {'otp': otp}).json
+        app.post('/login', {'otp': otp})
+        status = app.get('/status').json
         assert status['username'] == 'user1'
-        app.get('/yubiauth/client/logout')
+        app.get('/logout')
 
-    assert not app.post('/yubiauth/client/authenticate', {'otp': otp},
+    assert not app.post('/authenticate', {'otp': otp},
                         status=400).json
--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -20,130 +20,130 @@ def setup():
 
 def test_create_user():
     resp = app.post(
-        '/yubiauth/core/users',
+        '/users',
         {'username': 'user1', 'password': 'foo'},
         status=201
     )
     user = app.get(resp.location).json
     assert user['name'] == 'user1'
     assert user['id'] == 1
-    app.post('/yubiauth/core/users', {'username': 'user2', 'password': 'bar'},
+    app.post('/users', {'username': 'user2', 'password': 'bar'},
              status=201)
-    app.post('/yubiauth/core/users', {'username': 'user3', 'password': 'baz'},
+    app.post('/users', {'username': 'user3', 'password': 'baz'},
              status=201)
 
-    assert len(app.get('/yubiauth/core/users').json) == 3
+    assert len(app.get('/users').json) == 3
 
 
 def test_delete_user_by_id():
     resp = app.post(
-        '/yubiauth/core/users',
+        '/users',
         {'username': 'redshirt', 'password': 'llap'},
         status=201
     )
     id = app.get(resp.location).json['id']
 
-    app.delete('/yubiauth/core/users/%d' % id)
-    app.get('/yubiauth/core/users/%d' % id, status=404)
+    app.delete('/users/%d' % id)
+    app.get('/users/%d' % id, status=404)
 
 
 def test_delete_user_by_name():
     resp = app.post(
-        '/yubiauth/core/users',
+        '/users',
         {'username': 'redshirt', 'password': 'llap'},
         status=201
     )
     name = app.get(resp.location).json['name']
 
-    app.delete('/yubiauth/core/users/%s' % name.encode('ascii'))
-    app.get('/yubiauth/core/users/%s' % name, status=404)
+    app.delete('/users/%s' % name.encode('ascii'))
+    app.get('/users/%s' % name, status=404)
 
 
 def test_delete_user_by_post():
     resp = app.post(
-        '/yubiauth/core/users',
+        '/users',
         {'username': 'redshirt', 'password': 'llap'},
         status=201
     )
     id = app.get(resp.location).json['id']
 
-    app.post('/yubiauth/core/users/%d/delete' % id)
-    app.get('/yubiauth/core/users/%d' % id, status=404)
+    app.post('/users/%d/delete' % id)
+    app.get('/users/%d' % id, status=404)
 
 
 def test_get_user_by_id():
-    user = app.get('/yubiauth/core/users/1', status=200).json
+    user = app.get('/users/1', status=200).json
     assert user['id'] == 1
     assert user['name'] == 'user1'
 
 
 def test_get_user_by_name():
-    user = app.get('/yubiauth/core/users/user1').json
+    user = app.get('/users/user1').json
     assert user['id'] == 1
     assert user['name'] == 'user1'
 
 
 def test_validate_password_get():
-    status = app.get('/yubiauth/core/users/user1/validate?password=foo'
+    status = app.get('/users/user1/validate?password=foo'
                      ).json
     assert status['valid_password']
 
 
 def test_validate_password_post():
     status = app.post(
-        '/yubiauth/core/users/user1/validate',
+        '/users/user1/validate',
         {'password': 'foo'}
     ).json
     assert status['valid_password']
 
 
 def test_reset_password():
-    status = app.get('/yubiauth/core/users/user1/validate?password=foo').json
+    status = app.get('/users/user1/validate?password=foo').json
     assert status['valid_password']
-    app.post('/yubiauth/core/users/1/reset', {'password': 'foobar'})
+    app.post('/users/1/reset', {'password': 'foobar'})
 
-    status = app.get('/yubiauth/core/users/user1/validate?password=foo').json
+    status = app.get('/users/user1/validate?password=foo').json
     assert not status['valid_password']
 
-    status = app.get('/yubiauth/core/users/user1/validate?password=foobar'
+    status = app.get('/users/user1/validate?password=foobar'
                      ).json
     assert status['valid_password']
 
 
 def test_rename_user():
-    app.post('/yubiauth/core/users/user1/rename', {'username': 'user2'},
+    app.post('/users/user1/rename', {'username': 'user2'},
              status=400)
-    app.post('/yubiauth/core/users/user1/rename', {'username': 'new_user'})
-    app.get('/yubiauth/core/users/user1', status=404)
-    app.get('/yubiauth/core/users/new_user')
-    app.post('/yubiauth/core/users/new_user/rename', {'username': 'user1'})
-    app.get('/yubiauth/core/users/user1')
-    app.get('/yubiauth/core/users/new_user', status=404)
+    app.post('/users/user1/rename', {'username': 'new_user'})
+    app.get('/users/user1', status=404)
+    app.get('/users/new_user')
+    app.post('/users/new_user/rename', {'username': 'user1'})
+    app.get('/users/user1')
+    app.get('/users/new_user', status=404)
 
 
 def test_create_user_with_existing_username():
-    app.post('/yubiauth/core/users', {
+    app.post('/users', {
              'username': 'user1', 'password': 'bar'}, status=400)
 
 
 def test_validate_with_invalid_username():
-    app.post('/yubiauth/core/users/notauser/validate',
+    app.post('/users/notauser/validate',
              {'password': 'foo'}, status=404)
 
-    app.post('/yubiauth/core/users/notauser/validate', status=404)
+    app.post('/users/notauser/validate', status=404)
 
 
 def test_validate_with_invalid_password():
-    status = app.post('/yubiauth/core/users/user1/validate',
+    status = app.post('/users/user1/validate',
                       {'password': 'wrongpassword'}).json
     assert not status['valid_password']
 
-    status = app.post('/yubiauth/core/users/user1/validate').json
+    status = app.post('/users/user1/validate').json
     assert not status['valid_password']
 
 
 def test_get_user_by_invalid_username():
-    assert app.get('/yubiauth/core/users/notauser', status=404)
+    assert app.get('/users/notauser', status=404)
 
 
 # YubiKeys
@@ -155,46 +155,46 @@ PREFIX_3 = 'cccccccccccf'
 
 
 def test_bind_yubikeys():
-    yubikeys = app.get('/yubiauth/core/users/1/yubikeys').json
+    yubikeys = app.get('/users/1/yubikeys').json
     assert len(yubikeys) == 0
 
-    app.post('/yubiauth/core/users/1/yubikeys', {'yubikey': PREFIX_1})
-    yubikeys = app.get('/yubiauth/core/users/1/yubikeys').json
+    app.post('/users/1/yubikeys', {'yubikey': PREFIX_1})
+    yubikeys = app.get('/users/1/yubikeys').json
     assert yubikeys == [PREFIX_1]
 
-    app.post('/yubiauth/core/users/1/yubikeys', {'yubikey': PREFIX_2})
-    app.post('/yubiauth/core/users/2/yubikeys', {'yubikey': PREFIX_2})
+    app.post('/users/1/yubikeys', {'yubikey': PREFIX_2})
+    app.post('/users/2/yubikeys', {'yubikey': PREFIX_2})
 
-    yubikeys = app.get('/yubiauth/core/users/1/yubikeys').json
+    yubikeys = app.get('/users/1/yubikeys').json
     assert sorted(yubikeys) == sorted([PREFIX_1, PREFIX_2])
 
-    user = app.get('/yubiauth/core/users/1').json
+    user = app.get('/users/1').json
     assert sorted(user['yubikeys']) == sorted([PREFIX_1, PREFIX_2])
 
-    user = app.get('/yubiauth/core/users/2').json
+    user = app.get('/users/2').json
     assert user['yubikeys'] == [PREFIX_2]
 
 
 def test_show_yubikey():
-    yubikey = app.get('/yubiauth/core/users/1/yubikeys/%s' % PREFIX_1).json
+    yubikey = app.get('/users/1/yubikeys/%s' % PREFIX_1).json
     assert yubikey['enabled']
     assert yubikey['prefix'] == PREFIX_1
 
 
 def test_show_yubikey_for_wrong_user():
-    app.get('/yubiauth/core/users/2/yubikeys/%s' % PREFIX_1, status=404)
+    app.get('/users/2/yubikeys/%s' % PREFIX_1, status=404)
 
 
 def test_unbind_yubikeys():
-    app.delete('/yubiauth/core/users/1/yubikeys/%s' % PREFIX_1)
-    yubikeys = app.get('/yubiauth/core/users/1/yubikeys').json
+    app.delete('/users/1/yubikeys/%s' % PREFIX_1)
+    yubikeys = app.get('/users/1/yubikeys').json
     assert yubikeys == [PREFIX_2]
 
 
 def test_find_user_by_yubikey():
-    app.post('/yubiauth/core/users/2/yubikeys', {'yubikey': PREFIX_3})
+    app.post('/users/2/yubikeys', {'yubikey': PREFIX_3})
 
-    user = app.get('/yubiauth/core/user?yubikey=%s' % PREFIX_3).json
+    user = app.get('/user?yubikey=%s' % PREFIX_3).json
     assert user['id'] == 2
     assert PREFIX_3 in user['yubikeys']
 
@@ -203,43 +203,43 @@ def test_find_user_by_yubikey():
 
 
 def test_assign_attributes():
-    app.post('/yubiauth/core/users/1/attributes', {
+    app.post('/users/1/attributes', {
              'key': 'attr1', 'value': 'val1'})
-    app.post('/yubiauth/core/users/1/attributes', {
+    app.post('/users/1/attributes', {
              'key': 'attr2', 'value': 'val2'})
 
-    attributes = app.get('/yubiauth/core/users/1/attributes').json
+    attributes = app.get('/users/1/attributes').json
     assert attributes['attr1'] == 'val1'
     assert attributes['attr2'] == 'val2'
     assert len(attributes) == 2
 
-    user = app.get('/yubiauth/core/users/1').json
+    user = app.get('/users/1').json
     assert user['attributes'] == attributes
 
 
 def test_find_user_by_attribute():
-    user1 = app.get('/yubiauth/core/user?attr1=val1&attr2=val2').json
+    user1 = app.get('/user?attr1=val1&attr2=val2').json
     assert user1['id'] == 1
 
-    user2 = app.post('/yubiauth/core/user', {
+    user2 = app.post('/user', {
                      'attr1': 'val1', 'attr2': 'val2'}).json
     assert user1 == user2
 
 
 def test_read_attribute():
-    value = app.get('/yubiauth/core/users/1/attributes/attr1').json
+    value = app.get('/users/1/attributes/attr1').json
     assert value == 'val1'
 
 
 def test_read_missing_attribute():
-    value = app.get('/yubiauth/core/users/1/attributes/foo').json
+    value = app.get('/users/1/attributes/foo').json
     assert not value
 
 
 def test_overwrite_attributes():
-    app.post('/yubiauth/core/users/1/attributes', {
+    app.post('/users/1/attributes', {
              'key': 'attr1', 'value': 'newval'})
-    attributes = app.get('/yubiauth/core/users/1/attributes').json
+    attributes = app.get('/users/1/attributes').json
 
     assert attributes['attr1'] == 'newval'
     assert attributes['attr2'] == 'val2'
@@ -247,13 +247,13 @@ def test_overwrite_attributes():
 
 
 def test_unset_attributes():
-    app.delete('/yubiauth/core/users/1/attributes/attr1')
-    attributes = app.get('/yubiauth/core/users/1/attributes').json
+    app.delete('/users/1/attributes/attr1')
+    attributes = app.get('/users/1/attributes').json
 
     assert attributes['attr2'] == 'val2'
     assert len(attributes) == 1
 
-    value = app.get('/yubiauth/core/users/1/attributes/attr1').json
+    value = app.get('/users/1/attributes/attr1').json
     assert not value
 
 
@@ -280,11 +280,11 @@ def _basic_attribute_test(base_url):
 
 
 def test_user_attributes():
-    _basic_attribute_test('/yubiauth/core/users/1/attributes')
-    _basic_attribute_test('/yubiauth/core/users/2/attributes')
+    _basic_attribute_test('/users/1/attributes')
+    _basic_attribute_test('/users/2/attributes')
 
 
 def test_yubikey_attributes():
-    _basic_attribute_test('/yubiauth/core/yubikeys/%s/attributes' % PREFIX_1)
+    _basic_attribute_test('/yubikeys/%s/attributes' % PREFIX_1)
     _basic_attribute_test(
-        '/yubiauth/core/users/2/yubikeys/%s/attributes' % PREFIX_2)
+        '/users/2/yubikeys/%s/attributes' % PREFIX_2)
