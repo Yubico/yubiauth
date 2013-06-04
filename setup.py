d4f4a34286ac6d38d65e855831d4659d3c8a4eb7
Dain Nilsson <dain@yubico.com>
2013-06-04 13:59:54 +0200

    Bumped version post release.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if 'hsm' in sys.argv:
 
 setup(
     name='yubiauth',
-    version='0.3.2',
+    version='0.3.3',
     author='Dain Nilsson',
     author_email='dain@yubico.com',
     maintainer='Yubico Open Source Maintainers',
